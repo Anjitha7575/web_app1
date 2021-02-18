3a4e828ac92f9d3d08d978d7b8bfbde91ca881cb
Anjitha <anjitha.d@repute.net>
2021-02-18 19:24:34 +0530

    dfdf

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
           id="1"
           name="my_frame"
           title="Title"
-          src="http://localhost:3000/gateway?order_id=5a8c56-48f-0eb8-f473-c6deffb4d110&token=61d7c2dd-a50d-4ac5-8199-3f336fbb3aff&email=anjitha.d@repute.net&phone_number=7760426651"
-          style="width: 100%; height: 100%; margin: 0px; padding: 0px;"
+          src="https://app.forms.dev.repute.net/anjtest_dev/dev3"
+          style={{ width: '100%', height: '100%', margin: '0px', padding: '0px' }}
           align="center"
           scrolling="yes"
         ></iframe>
