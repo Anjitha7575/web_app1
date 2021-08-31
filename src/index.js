0b11568c54435c9412fec8dceb8e770e5e0050fc
Anjitha <anjitha.d@repute.net>
2021-08-31 11:47:42 +0530

    Added

--- src/index.js
+++ src/index.js
@@ -1,28 +1,28 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import {Provider} from 'react-redux';
-// import './index.css';
-// import App from './App';
-// import {createStore, compose, applyMiddleware} from 'redux';
-// import thunk from 'redux-thunk';
-// import * as serviceWorker from './serviceWorker';
-// import rootReducer from './reducers';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import './index.css';
+import App from './App';
+import {createStore, compose, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// const middlewares = compose(
-//   applyMiddleware(thunk),
-//   window.devToolsExtension ? window.devToolsExtension({ name: 'lordApp' }) : f => f
-// );
+const middlewares = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension({ name: 'lordApp' }) : f => f
+);
 
-// const appStore = createStore(rootReducer, undefined, middlewares);
+const appStore = createStore(rootReducer, undefined, middlewares);
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <Provider store={appStore}>
-//     <App />
-//     </Provider>
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={appStore}>
+    <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// serviceWorker.unregister();
+serviceWorker.unregister();
